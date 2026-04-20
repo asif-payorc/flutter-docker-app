429add387c0a183dc8b83f4869d8828b5bba5c58
asif-payorc <asif.ali@payorc.com>
2026-04-20 18:26:24 +0530

    hot fixes

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Row(
-
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
